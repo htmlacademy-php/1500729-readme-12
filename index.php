03f2c445dfbc79dcc1fc5154ba44fa5e3966b464
Алексей <anonymous.aleksey@gmail.com>
2020-10-02 15:08:30 +0300

    выполнил задание

--- index.php
+++ index.php
@@ -203,69 +203,87 @@ $user_name = 'Алексей Онипко'; // укажите здесь ваш�
             </div>
         </div>
         <div class="popular__posts">
-            <div class="visually-hidden" id="donor">
-                <!--содержимое для поста-цитаты-->
+        <?php $popular_posts = [
+            [
+                'header' => 'Цитата',
+                'type' => 'post-quote',
+                'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+                'user-name' => 'Лариса',
+                'userpic' => 'userpic-larisa-small.jpg'
+            ],
+            [
+                'header' => 'Игра престолов',
+                'type' => 'post-text',
+                'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+                'user-name' => 'Владик',
+                'userpic' => 'userpic.jpg' 
+            ],
+            [
+                'header' => 'Наконец, обработал фотки!',
+                'type' => 'post-photo',
+                'content' => 'rock-medium.jpg',
+                'user-name' => 'Виктор',
+                'userpic' => 'userpic-mark.jpg' 
+            ],
+            [
+                'header' => 'Моя мечта',
+                'type' => 'post-photo',
+                'content' => 'coast-medium.jpg',
+                'user-name' => 'Лариса',
+                'userpic' => 'userpic-larisa-small.jpg' 
+            ],
+            [
+                'header' => 'Лучшие курсы',
+                'type' => 'post-link',
+                'content' => 'www.htmlacademy.ru',
+                'user-name' => 'Владик',
+                'userpic' => 'userpic.jpg' 
+            ]
+            ];
+        ?>
+        <?php foreach ($popular_posts as $post): ?>
+            <article class="popular__post post <?=$post['type'];?>">
+                <header class="post__header">
+                    <h2><?=$post['header'];?></h2>
+                </header>
+                <div class="post__main">
+                    <?php if($post['type'] == 'post-quote'): ?>
                 <blockquote>
                     <p>
-                        <!--здесь текст-->
+                        <?=$post['content'];?>
                     </p>
                     <cite>Неизвестный Автор</cite>
                 </blockquote>
-
-                <!--содержимое для поста-ссылки-->
-                <div class="post-link__wrapper">
-                    <a class="post-link__external" href="http://" title="Перейти по ссылке">
+                    <?php elseif($post['type'] == 'post-text'): ?>
+                        <p><?=$post['content'];?></p>
+                    <?php elseif($post['type'] == 'post-photo'): ?>
+                        <div class="post-photo__image-wrapper">
+                        <img src="img/<?=$post['content'];?>" alt="Фото от пользователя" width="360" height="240">
+                        </div> 
+                    <?php elseif($post['type'] == 'post-link'): ?> 
+                        <div class="post-link__wrapper">
+                        <a class="post-link__external" href="http://" title="Перейти по ссылке">
                         <div class="post-link__info-wrapper">
                             <div class="post-link__icon-wrapper">
                                 <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
                             </div>
                             <div class="post-link__info">
-                                <h3><!--здесь заголовок--></h3>
+                                <h3><?=$post['header'];?></h3>
                             </div>
                         </div>
-                        <span><!--здесь ссылка--></span>
+                        <span><?=$post['content'];?></span>
                     </a>
-                </div>
-
-                <!--содержимое для поста-фото-->
-                <div class="post-photo__image-wrapper">
-                    <img src="img/" alt="Фото от пользователя" width="360" height="240">
-                </div>
-
-                <!--содержимое для поста-видео-->
-                <div class="post-video__block">
-                    <div class="post-video__preview">
-                        <?=embed_youtube_cover(/* вставьте ссылку на видео */); ?>
-                        <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
-                    </div>
-                    <a href="post-details.html" class="post-video__play-big button">
-                        <svg class="post-video__play-big-icon" width="14" height="14">
-                            <use xlink:href="#icon-video-play-big"></use>
-                        </svg>
-                        <span class="visually-hidden">Запустить проигрыватель</span>
-                    </a>
-                </div>
-
-                <!--содержимое для поста-текста-->
-                <p><!--здесь текст--></p>
-            </div>
-
-            <article class="popular__post post">
-                <header class="post__header">
-                    <h2><!--здесь заголовок--></h2>
-                </header>
-                <div class="post__main">
-                    <!--здесь содержимое карточки-->
+                         </div>   
+                    <?php endif; ?>
                 </div>
                 <footer class="post__footer">
                     <div class="post__author">
                         <a class="post__author-link" href="#" title="Автор">
                             <div class="post__avatar-wrapper">
-                                <!--укажите путь к файлу аватара-->
-                                <img class="post__author-avatar" src="img/" alt="Аватар пользователя">
+                                <img class="post__author-avatar" src="img/<?=$post['userpic'];?>" alt="Аватар пользователя">
                             </div>
                             <div class="post__info">
-                                <b class="post__author-name"><!--здесь имя пользоателя--></b>
+                                <b class="post__author-name"><?=$post['user-name'];?></b>
                                 <time class="post__time" datetime="">дата</time>
                             </div>
                         </a>
@@ -293,6 +311,7 @@ $user_name = 'Алексей Онипко'; // укажите здесь ваш�
                     </div>
                 </footer>
             </article>
+        <?php endforeach; ?>
         </div>
     </div>
 </section>
