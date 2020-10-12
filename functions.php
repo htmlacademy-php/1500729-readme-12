562e12631f8e4f11d10b771d587e3164dab8e208
Алексей <anonymous.aleksey@gmail.com>
2020-10-12 13:33:26 +0300

    написал код выводящий строку с нужным текстом, понять бы где его разместить

--- functions.php
+++ functions.php
@@ -50,3 +50,74 @@ function format_text($string, $simbols = 300) {
 
         return $text;
     }
+    /**
+ * @param $index
+ * @return false|string
+ */
+function generate_random_date($index)
+{
+    $deltas = [['minutes' => 59], ['hours' => 23], ['days' => 6], ['weeks' => 4], ['months' => 11]];
+    $dcnt = count($deltas);
+
+    if ($index < 0) {
+        $index = 0;
+    }
+
+    if ($index >= $dcnt) {
+        $index = $dcnt - 1;
+    }
+
+    $delta = $deltas[$index];
+    $timeval = rand(1, current($delta));
+    $timename = key($delta);
+
+    $ts = strtotime("$timeval $timename ago");
+    $dt = date('Y-m-d H:i:s', $ts);
+
+    return $dt;
+};
+/**
+ * Возвращает корректную форму множественного числа
+ * Ограничения: только для целых чисел
+ *
+ * Пример использования:
+ * $remaining_minutes = 5;
+ * echo "Я поставил таймер на {$remaining_minutes} " .
+ *     get_noun_plural_form(
+ *         $remaining_minutes,
+ *         'минута',
+ *         'минуты',
+ *         'минут'
+ *     );
+ * Результат: "Я поставил таймер на 5 минут"
+ *
+ * @param int $number Число, по которому вычисляем форму множественного числа
+ * @param string $one Форма единственного числа: яблоко, час, минута
+ * @param string $two Форма множественного числа для 2, 3, 4: яблока, часа, минуты
+ * @param string $many Форма множественного числа для остальных чисел
+ *
+ * @return string Рассчитанная форма множественнго числа
+ */
+function get_noun_plural_form(int $number, string $one, string $two, string $many): string
+{
+    $number = (int)$number;
+    $mod10 = $number % 10;
+    $mod100 = $number % 100;
+
+    switch (true) {
+        case ($mod100 >= 11 && $mod100 <= 20):
+            return $many;
+
+        case ($mod10 > 5):
+            return $many;
+
+        case ($mod10 === 1):
+            return $one;
+
+        case ($mod10 >= 2 && $mod10 <= 4):
+            return $two;
+
+        default:
+            return $many;
+    }
+}
--- index.php
+++ index.php
@@ -9,6 +9,44 @@ $layout = include_template('layout.php',
                           'title' => "readme: популярное"]);
 
 print($layout);
-    
-
-
+$count = count($popular_posts);
+$index = 0;
+while ($index < $count) {
+    $random_date = generate_random_date ($index);
+    $post_date = strtotime ($random_date);
+    $diff_time = time() - $post_date;
+    print("$diff_time ");
+    switch(true) {
+        case ($diff_time < 3600):
+            $time_of_post = $diff_time / 60;
+            $time_of_post = floor($time_of_post);
+            $time_of_post = "{$time_of_post}"." " . get_noun_plural_form($time_of_post, 'минуту', 'минуты', 'минут') . " " . "назад";
+        print($time_of_post);
+        break;
+        case ($diff_time >= 3600 && $diff_time < 86400):
+            $time_of_post = $diff_time / 3600;
+            $time_of_post = floor($time_of_post);
+            $time_of_post = "{$time_of_post}"." " . get_noun_plural_form($time_of_post, 'час', 'часа', 'часов') . " " . "назад";
+            print($time_of_post);
+        break;
+        case ($diff_time >= 86400 && $diff_time < 604800):
+            $time_of_post = $diff_time / 86400;
+            $time_of_post = floor($time_of_post);
+            $time_of_post = "{$time_of_post}"." " . get_noun_plural_form($time_of_post, 'день', 'дня', 'дней') . " " . "назад";
+            print($time_of_post);
+        break;
+        case ($diff_time >= 604800 && $diff_time < 3024000 ):
+            $time_of_post = $diff_time / 604800;
+            $time_of_post = floor($time_of_post);
+            $time_of_post = "{$time_of_post}"." " . get_noun_plural_form($time_of_post, 'неделя', 'недели', 'недель') . " " . "назад";
+            print($time_of_post);
+        break;
+        case ($diff_time >= 3024000):
+            $time_of_post = $diff_time / 3024000;
+            $time_of_post = floor($time_of_post);
+            $time_of_post = "{$time_of_post}"." " . get_noun_plural_form($time_of_post, 'месяц', 'месяца', 'месяцев') . " " . "назад";
+            print($time_of_post);
+        break;
+    }
+    $index++;
+}
