2b10b3b9994069198ef36bf1941776c2b1265799
Алексей <anonymous.aleksey@gmail.com>
2020-10-25 20:25:50 +0300

    добавил промежуточную таблицу, подкорректировал порядок запросов на создание, изменил разер

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ DEFAULT COLLATE UTF8_GENERAL_CI;
 
 USE readme;
 
+-- таблица пользователей
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(64) NOT NULL UNIQUE,
@@ -13,22 +14,31 @@ CREATE TABLE users (
     avatar VARCHAR(64)
 );
 
+-- таблица типов постов
+CREATE TABLE post_types (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name_type VARCHAR(32) UNIQUE,
+    name_class_icons VARCHAR(32) UNIQUE 
+);
+
+-- таблица постов
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     title VARCHAR(128),
     content_text TEXT,
-    author quotes VARCHAR(64),
+    author_quotes VARCHAR(64),
     picture VARCHAR(64),
     video VARCHAR(64),
-    href TEXT(128),
+    href VARCHAR(128),
     count_views INT,
     user_id INT NOT NULL,
     post_type INT NOT NULL,
-    hashtag VARCHAR(128),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (post_type) REFERENCES post_types (id)
 );
 
+-- таблица комментариев
 CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -39,21 +49,28 @@ CREATE TABLE comments (
     FOREIGN KEY (post_id) REFERENCES posts (id)
 );
 
+-- таблица лайков
 CREATE TABLE likes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     post_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (post_id) REFERENCES posts (id)
 );
 
+
+-- таблица подписок
 CREATE TABLE subscriptions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     autor_id INT NOT NULL,
     user_id INT NOT NULL,
     FOREIGN KEY (autor_id) REFERENCES users (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
+-- таблица сообщений
 CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     message_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     message_content TEXT(10000),
     sender_id INT NOT NULL,
@@ -62,20 +79,23 @@ CREATE TABLE messages (
     FOREIGN KEY (recipient_id) REFERENCES users (id)
 );
 
+
+-- таблица хештегов
 CREATE TABLE hashtags (
-    name_hashtags VARCHAR(128) UNIQUE
-);
-
-CREATE TABLE post_types (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name_type VARCHAR(32) UNIQUE,
-    name_class_icons VARCHAR(32) UNIQUE 
+    name_hashtags VARCHAR(64) UNIQUE
 );
 
-ALTER TABLE posts ADD FOREIGN KEY (post_type) REFERENCES post_types (id);
+-- промежуточная таблица связь "многие-ко-многим" (посты и хештеги)
+CREATE TABLE posts_hashtags (
+    post_id INT,
+    hashtag_id INT,
+    FOREIGN KEY (post_id) REFERENCES posts (id),
+    FOREIGN KEY (hashtag_id) REFERENCES hashtags (id),
+    PRIMARY KEY (post_id, hashtag_id)
+);
 
+-- дополнительные индексы
 CREATE INDEX u_name ON users (login);
-
 CREATE INDEX p_title ON posts (title);
-
 CREATE INDEX m_text ON hashtags (name_hashtags);
