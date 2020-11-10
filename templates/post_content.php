00375e6fef50380c4aade6b4745a511284a95741
Алексей <anonymous.aleksey@gmail.com>
2020-11-10 08:18:35 +0300

    применил размер к фото

--- templates/post_content.php
+++ templates/post_content.php
@@ -43,16 +43,6 @@
         </div>
       </div>
 
-      <?php elseif ($post_page['post_type'] == 1): ?>
-        <div class="post__main">
-                  <blockquote>
-                    <p>
-                       <?= $post_page['content_text']; ?>
-                    </p>
-                    <cite><?= $post_page['author_quotes'] ?></cite>
-                  </blockquote>
-        </div>
-
       <?php elseif ($post_page['post_type'] == 4): ?>
         <div class="post__main">
                   <div class="post-link__wrapper">
@@ -71,6 +61,16 @@
                     </a>
                   </div>
          </div>
+
+      <?php elseif ($post_page['post_type'] == 1): ?>
+        <div class="post__main">
+                  <blockquote>
+                    <p>
+                       <?= $post_page['content_text']; ?>
+                    </p>
+                    <cite><?= $post_page['author_quotes'] ?></cite>
+                  </blockquote>
+        </div>
 
         <?php endif; ?>
         
@@ -106,7 +106,7 @@
           <div class="comments">
             <form class="comments__form form" action="#" method="post">
               <div class="comments__my-avatar">
-                <img class="comments__picture" src="/img/userpic-medium.jpg" alt="Аватар пользователя">
+                <img class="comments__picture" src="/img/userpic.jpg" alt="Аватар пользователя">
               </div>
               <div class="form__input-section form__input-section--error">
                 <textarea class="comments__textarea form__textarea form__input" placeholder="Ваш комментарий"></textarea>
@@ -124,7 +124,7 @@
                 <li class="comments__item user">
                   <div class="comments__avatar">
                     <a class="user__avatar-link" href="#">
-                      <img class="comments__picture" src="/img/userpic-larisa.jpg" alt="Аватар пользователя">
+                      <img class="comments__picture" width="40" src="img/userpic-larisa.jpg" alt="Аватар пользователя">
                     </a>
                   </div>
                   <div class="comments__info">
@@ -142,7 +142,7 @@
                 <li class="comments__item user">
                   <div class="comments__avatar">
                     <a class="user__avatar-link" href="#">
-                      <img class="comments__picture" src="/img/userpic-larisa.jpg" alt="Аватар пользователя">
+                      <img class="comments__picture" width="40" src="/img/userpic-larisa.jpg" alt="Аватар пользователя">
                     </a>
                   </div>
                   <div class="comments__info">
